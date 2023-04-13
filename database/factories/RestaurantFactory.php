06dd61076a7125f8b1e8e0a02393ca0c1b3ae0e3
carlos herrera <carlos.herrera@patterson.agency>
2023-04-13 12:03:27 +0200

    Migraciones, factories y seeders de user y restaurante implementado; get all y get one en el controlador api de restaurante

---
+++ database/factories/RestaurantFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Restaurant>
+ */
+class RestaurantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'food' => fake()->word(),
+            'location' => fake()->city(),
+            'name' => fake()->company(),
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Restaurant;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        $users = User::factory(10)->create();
+        foreach($users as $user){
+            if($user->owner==true){
+                Restaurant::factory()->for($user)->create();
+            }
+        }
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
