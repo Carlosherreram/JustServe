62c0aed21b65a70f527aa353b700971d3b4f0a9b
Huberto Martin Reina <huberto.martin@w2m.com>
2023-05-29 12:22:03 +0200

    bug

--- src/app/components/reserva/reserva.component.ts
+++ src/app/components/reserva/reserva.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ReservaResponse } from 'src/app/models/reserva-response';
 import { Reserva } from 'src/app/models/reserva.model';
+import { AuthService } from 'src/app/services/auth.service';
 import { DataService } from 'src/app/services/data.service';
 import { ReservaService } from 'src/app/services/reserva.service';
 
@@ -14,19 +15,23 @@ export class ReservaComponent implements OnInit {
   public date: Date = new Date()
   public logged: boolean = true
   public loaded = false
+  public in = this.AUTH_SERVICE.isLoggedIn
+  public MSG = ''
 
   constructor(private reservaService: ReservaService,
-    private dataService: DataService) {
+    private dataService: DataService,
+    private AUTH_SERVICE: AuthService) {
   }
 
   ngOnInit(): void {
-    this.reservaService.getReserves().subscribe((reserve: ReservaResponse[]) => {
-      if (reserve && typeof reserve === 'object') {
-        this.reservas = Object.values(reserve);
-      }
-      this.loaded = true
-      console.log(this.reservas)
-    })
+    if (this.in) {
+      this.reservaService.getReserves().subscribe((reserve: ReservaResponse[]) => {
+        if (reserve && typeof reserve === 'object') {
+          this.reservas = Object.values(reserve);
+        }
+        this.loaded = true
+      })
+    }
 
   }
 
@@ -44,6 +49,20 @@ export class ReservaComponent implements OnInit {
 
   public deleteReserve(idReserve: number) {
     this.dataService.deleteReserves(idReserve, this.dataService.token).subscribe(() => {
+      this.reservaService.getReserves().subscribe((reserve: ReservaResponse[]) => {
+        if (reserve && typeof reserve === 'object') {
+          this.reservas = Object.values(reserve);
+        }
+      })
+      const notification = document.querySelector('.notification');
+
+      if (notification) {
+        notification.classList.add('show');
+        setTimeout(() => {
+          notification.classList.remove('show');
+        }, 3000);
+      }
+      this.MSG = `SE HA ELIMINADO LA RESERVA`;
 
     });
 
